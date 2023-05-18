fd13fd08addc65680b9fa3fda013de330ba09a24
javafx <abhi267266@gmail.com>
2023-05-18 16:41:10 +0530

    created file structure for the todolistproject or major project 1 for coding ninja also create frontend for it and createde route for home page

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const port = 8000;
+const app = express();
+
+
+app.use('/', require('./routes'))
+app. set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.urlencoded());
+app.use(express.static('asset'));
+
+app.listen(8000, (err)=>{
+    if(err){
+        console.log("something went wrong connecting to server");
+    }
+
+    console.log(`Server is running in port ${port}`);
+});
